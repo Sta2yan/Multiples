facd43d8c5ea7e73c4d345eb53808056bc44cd09
Sta2yan <s2_stas@mail.ru>
2022-03-10 20:39:17 +0300

    Добавьте файлы проекта.

---
+++ Multiples/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Multiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maximumNumber = 27;
+            int startNumber = 3;
+            int multipleNumbers = 0;
+            int enlargedNumber = 3;
+
+            for (int i = startNumber; i <= maximumNumber; i += enlargedNumber)
+            {
+                ++multipleNumbers;
+            }
+
+            Console.WriteLine($"{multipleNumbers}");
+        }
+    }
+}
